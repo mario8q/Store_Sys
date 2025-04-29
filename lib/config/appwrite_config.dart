7e8cfa49a3a3e49b025be6de92ee8e6a04b937e0
Mario Santacruz <mariofernandosantacruzp@gmail.com>
2025-04-29 16:20:59 -0500

    Inicializando conexion de collection productos de appwrite

--- lib/config/appwrite_config.dart
+++ lib/config/appwrite_config.dart
@@ -2,6 +2,6 @@ class AppwriteConfig {
   static const String endpoint = 'https://fra.cloud.appwrite.io/v1';
   static const String projectId = '67feabe900336aa47e63';
   static const String databaseId = '67feb0500028c7a75ce8';
-  static const String productsCollectionId = 'products';
+  static const String productsCollectionId = '67feb07b001578d60c56';
   static const String productsBucketId = 'products';
 }
